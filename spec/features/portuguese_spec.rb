48ef9606137966a1da4d0197a184615b3f1da691
vandosant <sjskender@gmail.com>
2014-05-02 11:01:30 -0600

    All users can view portuguese content

--- spec/features/portuguese_spec.rb
+++ spec/features/portuguese_spec.rb
@@ -1,25 +1,8 @@
 require 'spec_helper'
 
 feature 'vocabulary' do
-  before do
-    visit '/'
-
-    click_link 'register'
-
-    fill_in 'email', with: 'alltest@example.com'
-    fill_in 'password', with: 'password'
-    click_button 'register'
-    click_link 'logout'
-  end
-
   scenario 'users can view vocab' do
     visit '/'
-
-    click_link 'login'
-
-    fill_in 'email', with: 'alltest@example.com'
-    fill_in 'password', with: 'password'
-    click_button 'login'
 
     click_link 'portuguese'
 
