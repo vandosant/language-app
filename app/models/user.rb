0194f879fe866802843da95673889e8ce7978bb5
Scott Skender <sjskender@gmail.com>
2014-05-31 10:14:53 -0600

    Improved error messages and refactored user specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
-  validates :email, :presence => {:message => 'email cannot be blank'},
-            :format => { :with => /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i, :message => 'invalid email address' },
-            :uniqueness => { :case_sensitive => false, :message => 'that email is already registered' }
-  validates :password, :length => { :minimum => 6, :message => 'password is too short' }
+  validates :email, :presence => {:message => 'cannot be blank'},
+            :format => { :with => /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i, :message => 'is invalid' },
+            :uniqueness => { :case_sensitive => false, :message => 'is already registered' }
+  validates :password, :length => { :minimum => 6, :message => 'is too short' }
   has_secure_password
 end
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'user authentication' do
-  before 'users can register and logout' do
+  scenario 'users can register and logout' do
     visit '/'
 
     click_link 'register'
@@ -19,6 +19,7 @@ feature 'user authentication' do
   end
 
   scenario 'users can login' do
+    user = User.create!(email: 'test@example.com', password: 'password')
     visit '/'
 
     click_link 'login'
@@ -41,8 +42,8 @@ feature 'user authentication' do
     fill_in 'Password', with: '123'
     click_button 'Register'
 
-    expect(page).to have_content 'invalid email address'
-    expect(page).to have_content 'password is too short'
+    expect(page).to have_content 'Email is invalid'
+    expect(page).to have_content 'Password is too short'
   end
 
   scenario 'users cannot login without registering first' do
@@ -58,6 +59,8 @@ feature 'user authentication' do
   end
 
   scenario 'users cannot login with an invalid password' do
+    user = User.create!(email: 'text@example.com', password: 'password')
+
     visit '/'
 
     click_link 'login'
